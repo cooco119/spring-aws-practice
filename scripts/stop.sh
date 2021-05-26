9bc9d896b8afcf0f409be631846aeaf59af51d23
Jaejun.Lee <cooco119@gmail.com>
2021-05-27 02:45:10 +0900

    Fix typo in stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -2,7 +2,7 @@
 
 ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
-source $(ABSDIR/profile.sh)
+source ${ABSDIR}/profile.sh
 
 IDLE_PORT=$(find_idle_port)
 
@@ -16,4 +16,4 @@ else
   echo "> kill -15 $IDLE_PID"
   kill -15 ${IDLE_PID}
   sleep 5
-if
+fi
